d7949c68fef31e28938a5054664f8dfa901c9cfb
eric <ericweix10@gmail.com>
2023-05-21 02:30:56 +0800

    Implement translating between 64-bit indexes and 32-bit seqnos

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -14,8 +14,9 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    uint64_t seqno_64 = n + isn.raw_value();
+    uint32_t seqno = seqno_64;
+    return WrappingInt32{seqno};
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +30,10 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint32_t offset = n.raw_value() - wrap(checkpoint, isn).raw_value();
+    uint64_t result = checkpoint + offset;
+    if (offset > (1u << 31) && checkpoint != 0ull) {
+        result -= (1ull << 32);
+    }
+    return result;
 }
